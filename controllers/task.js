b898274f2ab2677f0eecfca3efb2e7bcffc00391
Prins Kamariya <kamriyaprince@gmail.com>
2023-04-07 20:29:38 +0530

    Completed API of Todo

--- controllers/task.js
+++ controllers/task.js
@@ -2,61 +2,77 @@ import { ErrorHandler } from "../middlewares/error.js";
 import { Task } from "../model/task.js";
 
 export const newTask = async (req, res, next) => {
-    const { title, description } = req.body;
+    try {
+        const { title, description } = req.body;
 
-    await Task.create({
-        title,
-        description,
-        user: req.user,
-    });
-    // alternative of this create is :
-    // const task = new Task({title,description,user:req.user});
-    // await task.save();
+        await Task.create({
+            title,
+            description,
+            user: req.user,
+        });
+        // alternative of this create is :
+        // const task = new Task({title,description,user:req.user});
+        // await task.save();
 
-    res.status(201).json({
-        success: true,
-        message: "Task added Successfully",
-    });
+        res.status(201).json({
+            success: true,
+            message: "Task added Successfully",
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 export const getMyTask = async (req, res, next) => {
-    const userid = req.user._id;
+    try {
+        const userid = req.user._id;
 
-    const tasks = await Task.find({ user: userid });
+        const tasks = await Task.find({ user: userid });
 
-    res.status(200).json({
-        success: true,
-        tasks,
-    });
+        res.status(200).json({
+            success: true,
+            tasks,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 export const updateTask = async (req, res, next) => {
-    const id = req.params.id;
-    const task = await Task.findById(id);
+    try {
+        const id = req.params.id;
+        const task = await Task.findById(id);
 
-    if (!task)
-        return next(new ErrorHandler("Invalid ID or Task not found!", 404)); // if we use inbuilt Error class then we can only pass message like : return next(new Error("Invalid ID or Task not found!"))
+        if (!task)
+            return next(new ErrorHandler("Invalid ID or Task not found!", 404)); // if we use inbuilt Error class then we can only pass message like : return next(new Error("Invalid ID or Task not found!"))
 
-    task.isCompleted = !task.isCompleted;
+        task.isCompleted = !task.isCompleted;
 
-    await task.save();
+        await task.save();
 
-    res.status(200).json({
-        success: true,
-        message: "Task Updated!",
-    });
+        res.status(200).json({
+            success: true,
+            message: "Task Updated!",
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 export const deleteTask = async (req, res, next) => {
-    const { id } = req.params;
-    const task = await Task.findById(id);
+    try {
+        const { id } = req.params;
+        const task = await Task.findById(id);
 
-    if (!task) return next(new ErrorHandler("Invalid ID", 404));
+        if (!task) return next(new ErrorHandler("Invalid ID", 404));
 
-    await task.deleteOne();
+        await task.deleteOne();
 
-    res.status(200).json({
-        success: true,
-        message: "Task Deleted!",
-    });
+        res.status(200).json({
+            success: true,
+            message: "Task Deleted!",
+        });
+    } catch (error) {
+        next(error);
+    }
 };
--- controllers/user.js
+++ controllers/user.js
@@ -4,63 +4,72 @@ import jwt from "jsonwebtoken";
 import { sendCookie } from "../utils/features.js";
 
 export const register = async (req, res) => {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    let user = await User.findOne({ email });
+        let user = await User.findOne({ email });
 
-    if (user) {  // for maintain clean code here also use that : return next(new ErrorHandler("Invalid ID or Task not found!", 404)); our error handler insted of thia return statement below
-        return res.status(404).json({
-            success: false,
-            message: "User Already Exist",
+        if (user) {
+            // for maintain clean code here also use that : return next(new ErrorHandler("Invalid ID or Task not found!", 404)); our error handler insted of thia return statement below
+            return res.status(404).json({
+                success: false,
+                message: "User Already Exist",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        user = await User.create({
+            name: name,
+            email: email,
+            password: hashedPassword,
         });
+
+        // inside login also this token code will be there so repetation of code happes to we store this code inside somewhere and direct use it so in utils folder's features.js file we stored this.
+        // const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        //    // if we want with register login will also happens automatically then we can send cookie
+        // res.status(201)
+        //     .cookie("token", token, {
+        //         httpOnly: true,
+        //         maxAge: 15 * 60 * 1000, // this is same as expires
+        //     })
+        //     .json({
+        //         success: true,
+        //         message: "Registered Successfully",
+        //     });
+
+        sendCookie(user, res, "Registered Successfully", 201);
+    } catch {
+        next(error);
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    user = await User.create({
-        name: name,
-        email: email,
-        password: hashedPassword,
-    });
-
-    // inside login also this token code will be there so repetation of code happes to we store this code inside somewhere and direct use it so in utils folder's features.js file we stored this.
-    // const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-    //    // if we want with register login will also happens automatically then we can send cookie
-    // res.status(201)
-    //     .cookie("token", token, {
-    //         httpOnly: true,
-    //         maxAge: 15 * 60 * 1000, // this is same as expires
-    //     })
-    //     .json({
-    //         success: true,
-    //         message: "Registered Successfully",
-    //     });
-
-    sendCookie(user, res, "Registered Successfully", 201);
 };
 
 export const login = async (req, res, next) => {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).select("+password");
-    // inside schema of user.js in model folder we specify select: false in password, so whenever we get user's data here password will be not then we need to add it manually that's why .select(+password) written so prev whichever data we got plus password also we need inside user , if we write .select(password) then only password will be there in user
+        const user = await User.findOne({ email }).select("+password");
+        // inside schema of user.js in model folder we specify select: false in password, so whenever we get user's data here password will be not then we need to add it manually that's why .select(+password) written so prev whichever data we got plus password also we need inside user , if we write .select(password) then only password will be there in user
 
-    if (!user) {
-        return res.status(404).json({
-            success: false,
-            message: "Invalid Email or Password",
-        });
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid Email or Password",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid Email or Password",
+            });
+        }
+
+        sendCookie(user, res, `Welcome Back, ${user.name}`, 200);
+    } catch (error) {
+        next(error);
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        return res.status(404).json({
-            success: false,
-            message: "Invalid Email or Password",
-        });
-    }
-
-    sendCookie(user, res, `Welcome Back, ${user.name}`, 200);
 };
 
 export const getMyProfile = async (req, res) => {
